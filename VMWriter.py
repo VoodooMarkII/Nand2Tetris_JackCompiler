5dc8c72162727a4658a5179681309e45a43361ba
VoodooMarkII <alex431652@gmail.com>
2018-08-22 17:03:16 +0800

    Add comment.

--- VMWriter.py
+++ VMWriter.py
@@ -2,7 +2,16 @@ import os
 
 
 class VMWriter:
+
     def __init__(self, filename):
+        """
+        Open the vm file and get ready to write command to it.
+        Note:
+        1) It will override the current file if it already existed.
+        2) Use close() method after all manipulation.
+
+        :param filename: (string) The output file name without extension '.vm'.
+        """
         self.filename = filename + '.vm'
 
         if os.path.isfile(self.filename):
@@ -12,9 +21,9 @@ class VMWriter:
 
     def write_push(self, segment, index):
         """
-        :param segment: in list[constant, local, argument, static, this, that, pointer, temp]
-        :param index: Offset of given segment
-        :return: VM push command with given segment and index
+        :param segment: (string) In list['constant', 'local', 'argument', 'static', 'this', 'that, 'pointer', 'temp']
+        :param index: (int) Offset of given segment
+        :return: (string) VM push command with given segment and index
         """
         cmd = 'push {segment} {index}'.format(segment=segment, index=index)
         self.file.write(cmd + '\n')
@@ -22,9 +31,9 @@ class VMWriter:
 
     def write_pop(self, segment, index):
         """
-        :param segment: in list[local, argument, static, this, that, pointer, temp]
-        :param index: Offset of given segment
-        :return: VM pop command with given segment and index
+        :param segment: (string) In list['local', 'argument', 'static', 'this', 'that', 'pointer', 'temp']
+        :param index: (int) Offset of given segment
+        :return: (string) VM pop command with given segment and index
         """
         cmd = 'pop {segment} {index}'.format(segment=segment, index=index)
         self.file.write(cmd + '\n')
@@ -32,8 +41,8 @@ class VMWriter:
 
     def write_arithmetic(self, cmd):
         """
-        :param cmd: in the list[add, sub, neg, eq, gt, lt, and, or, not]
-        :return:  VM arithmetic command with given segment and index
+        :param cmd: (string) In the list['add', 'sub', 'neg', 'eq', 'gt', 'lt', 'and', 'or', 'not']
+        :return: (string) VM arithmetic command with given segment and index
         """
         self.file.write(cmd + '\n')
         return cmd
@@ -54,11 +63,21 @@ class VMWriter:
         return cmd
 
     def write_call(self, name, n_args):
+        """
+        :param name: (string) Function name
+        :param n_args: (int) The number of argument of callee
+        :return: (string) VM call command
+        """
         cmd = 'call {name} {n_args}'.format(name=name, n_args=n_args)
         self.file.write(cmd + '\n')
         return cmd
 
     def write_function(self, name, n_locals):
+        """
+        :param name: (string) Function name
+        :param n_locals: (int) The number of local variables of callee
+        :return: (string) VM function label
+        """
         cmd = 'function {name} {n_locals}'.format(name=name, n_locals=n_locals)
         self.file.write(cmd + '\n')
         return cmd
